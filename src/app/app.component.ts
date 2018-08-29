f5353e886f27d0365b98556f097854ab532ff330
Armando Olguin <olgcesar@gmail.com>
2018-08-29 10:17:03 -0500

    Funciona Menu

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -47,9 +47,9 @@ export class MyApp {
     });
   }
 
-  openPage(page) {
+  openPage(page2) {
     // Reset the content nav to have just this page
     // we wouldn't want the back button to show in this scenario
-    this.nav.setRoot(page.component);
+    this.nav.setRoot(page2.component);
   }
 }
